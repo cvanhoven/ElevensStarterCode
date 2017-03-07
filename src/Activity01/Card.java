99047d24ab923330de42ccb6bbe408fb74fbc9d2
2006893 <2006893@AL-105-33.student.acalanes.k12.ca.us>
2017-03-07 11:28:25 -0800

    Activity 1 constructor completed

--- src/Activity01/Card.java
+++ src/Activity01/Card.java
@@ -1,3 +1,5 @@
+package Activity01;
+
 /**
  * Card.java
  *
@@ -8,17 +10,17 @@ public class Card {
 	/**
 	 * String value that holds the suit of the card
 	 */
-	private String suit;
+	private final String suit;
 
 	/**
 	 * String value that holds the rank of the card
 	 */
-	private String rank;
+	private final String rank;
 
 	/**
 	 * int value that holds the point value.
 	 */
-	private int pointValue;
+	private final int pointValue;
 
 
    /**
@@ -32,7 +34,9 @@ public class Card {
 	 *                  containing the point value of the card
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		this.suit = cardSuit;
+                this.rank = cardRank;
+                this.pointValue = cardPointValue;
 	}
 
 
